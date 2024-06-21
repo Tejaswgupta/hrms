adfb4e5d13ac3f85a559cd3e591c3a18efacde9c
jaanhvisaxena <115059757+jaanhvisaxena@users.noreply.github.com>
2024-06-21 13:01:25 +0530

    allow shifts to be edited

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { AdminDashboard } from '@/components/component/admin-dashboard';
-import CsvReader from '@/components/component/CsvReader';
 const Admin: React.FC = () => {
   return (
     <div>
-\    <AdminDashboard/>
+     <AdminDashboard/>
     </div>
   );
 };
