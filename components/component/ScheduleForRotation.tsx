05bd5212191a1b2f8f313a231bec2e4fcee0e383
jaanhvisaxena <115059757+jaanhvisaxena@users.noreply.github.com>
2024-06-25 19:25:11 +0530

    save location edit

--- components/component/ScheduleForRotation.tsx
+++ components/component/ScheduleForRotation.tsx
@@ -10,10 +10,12 @@ interface Personnel {
   phone: string;
 }
 interface Junction {
+  id: number;
   name: string;
 }
 
 interface SubJunction {
+  id: number;
   name: string;
 }
 
@@ -34,9 +36,6 @@ const ScheduleForRotation: React.FC = () => {
   const [clickedCellIndex, setClickedCellIndex] = useState<number | null>(null);
   const [unAssigned, setUnAssigned] = useState([]);
 
-
-
-
   async function getSchedule(currentDate: Date) {
     const utcMidnight = new Date(
       Date.UTC(
@@ -49,7 +48,7 @@ const ScheduleForRotation: React.FC = () => {
     const { data: scheduleData, error } = await supabase
       .from("assignments")
       .select(
-        "junctions(name),sub_junctions(name),shift,start_date,end_date,personnel(name,role)"
+        "id,junctions(id,name),sub_junctions(id,name),shift,start_date,end_date,personnel(name,role)"
       )
       .lte("start_date", utcMidnight.toISOString())
       .gte("end_date", utcMidnight.toISOString());
@@ -106,135 +105,199 @@ const ScheduleForRotation: React.FC = () => {
     setFilterMenuOpen(true);
   };
 
-  const handleSelection = (selectedValue: string) => {
+  const handleSelection = async (selectedValue: string) => {
     if (clickedCellIndex !== null) {
       const updatedSchedule = [...schedule];
       const detail = updatedSchedule[clickedCellIndex];
+      let updatePayload = {};
+      
       if (detail.junctions) {
         detail.junctions.name = selectedValue;
+        updatePayload = { junction_id: getIdFromName(selectedValue, junctions) };
       } else if (detail.sub_junctions) {
         detail.sub_junctions.name = selectedValue;
+        updatePayload = { sub_junction_id: getIdFromName(selectedValue, subJunctions) };
       }
-      setSchedule(updatedSchedule);
-      setClickedCellIndex(null);
-      setCommandMenuOpen(false);
+
+      const { data, error } = await supabase
+        .from('assignments')
+        .update(updatePayload)
+        .eq('id', detail.id);
+
+      if (error) {
+        console.error('Error updating schedule:', error);
+      } else {
+        console.log('Schedule updated:', data);
+        setSchedule(updatedSchedule);
+        setClickedCellIndex(null);
+        setCommandMenuOpen(false);
+      }
     } else if (filterMenuOpen) {
       setFilterLocation(selectedValue);
       setFilterMenuOpen(false);
     }
   };
 
+  // Helper function to get the ID from the name
+  const getIdFromName = (name, list) => {
+    const item = list.find((item) => item.name === name);
+    return item ? item.id : null;
+  };
+
+  useEffect(() => {
+    // Fetch junctions and subJunctions data
+    async function fetchJunctions() {
+      const { data: junctionData, error: junctionError } = await supabase
+        .from('junctions')
+        .select('id, name');
+
+      if (junctionError) {
+        console.error('Error fetching junctions:', junctionError);
+      } else {
+        setJunctions(junctionData);
+      }
+    }
+
+    async function fetchSubJunctions() {
+      const { data: subJunctionData, error: subJunctionError } = await supabase
+        .from('sub_junctions')
+        .select('id, name');
+
+      if (subJunctionError) {
+        console.error('Error fetching sub junctions:', subJunctionError);
+      } else {
+        setSubJunctions(subJunctionData);
+      }
+    }
+
+    fetchJunctions();
+    fetchSubJunctions();
+  }, []);
+
   return (
     <div className="py-6">
       <Card id="rotation-schedule" className="mt-10">
-          <CardHeader>
-            <CardTitle>Rotation Schedule</CardTitle>
-          </CardHeader>
-          <CardContent>
-          <CommandMenu open={commandMenuOpen} setOpen={setCommandMenuOpen} onSelect={handleSelection} showOnlyjunction={true} showOnlySubjunction={true}/>
-      <CommandMenu open={filterMenuOpen} setOpen={setFilterMenuOpen} onSelect={handleSelection} showOnlyjunction={true} showOnlySubjunction={true} />
-      <div className="flex flex-col md:flex-row justify-between items-center mb-4">
-        <button
-          className="px-4 py-2 bg-blue-500 text-white rounded mb-2 md:mb-0"
-          onClick={() => setCurrentWeek(getPreviousWeek())}
-        >
-          Previous Week
-        </button>
-        <h3 className="text-xl mb-2 font-bold md:mb-0">
-          {currentWeek.toLocaleDateString("en-IN", {
-            year: "numeric",
-            month: "2-digit",
-            day: "2-digit",
-          })}
-          {" - "}
-          {getNextWeek().toLocaleDateString("en-IN", {
-            year: "numeric",
-            month: "2-digit",
-            day: "2-digit",
-          })}
-        </h3>
-        <button
-          className="px-4 py-2 bg-blue-500 text-white rounded"
-          onClick={() => setCurrentWeek(getNextWeek())}
-        >
-          Next Week
-        </button>
-      </div>
-      <div className="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-4 mb-4">
-        <label htmlFor="filterPosition" className="font-semibold mb-2 md:mb-0">
-          Filter by Position:
-        </label>
-        <select
-          id="filterPosition"
-          value={filterPosition ?? "All"}
-          onChange={(e) =>
-            setFilterPosition(e.target.value === "All" ? null : e.target.value)
-          }
-          className="p-2 border rounded"
-        >
-          <option value="All">All</option>
-          <option value="TSI">TSI</option>
-          <option value="Constable">Constable</option>
-          <option value="Home Guard">Home Guard</option>
-        </select>
-        <input
-          type="text"
-          placeholder="Filter by Name"
-          value={filterName ?? ""}
-          onChange={(e) =>
-            setFilterName(e.target.value === "" ? null : e.target.value)
-          }
-          className="p-2 border rounded"
-        />
-        <input
-          type="text"
-          placeholder="Filter by Location"
-          value={filterLocation ?? ""}
-          onClick={handleLocationFilterClick}
-          readOnly
-          className="p-2 border rounded cursor-pointer"
-        />
-      </div>
-      {filteredSchedule.length > 0 ? (
-        <div className="overflow-x-auto">
-          <table className="min-w-full bg-white border">
-            <thead>
-              <tr>
-                <th className="py-2 px-4 border-b text-left">Role</th>
-                <th className="py-2 px-4 border-b text-left">Name</th>
-              </tr>
-            </thead>
-            <tbody>
-              {filteredSchedule.map((detail, index) => (
-                <tr key={index}>
-                  <td className="py-2 px-4 border-b">{detail.personnel.role}</td>
-                  <td className="py-2 px-4 border-b">{detail.personnel.name}</td>
-                  <td className="py-2 px-4 border-b">
-                    <span
-                      onClick={() => handleLocationClick(index)}
-                      className="cursor-pointer text-blue-500"
-                    >
-                      {detail.junctions?.name ?? detail.sub_junctions?.name}
-                    </span>
-                  </td>
-                  <td className="py-2 px-4 border-b">{detail.shift ?? "All Day"}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <button
-            className="mt-4 px-4 py-2 bg-green-500 text-white rounded"
-            onClick={() => {}}
-          >
-            Save Changes
-          </button>
-        </div>
-      ) : (
-        <p>No schedule details available for the current week.</p>
-      )}
-          </CardContent>
-        </Card>
-
+        <CardHeader>
+          <CardTitle>Rotation Schedule</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <CommandMenu
+            open={commandMenuOpen}
+            setOpen={setCommandMenuOpen}
+            onSelect={handleSelection}
+            showOnlyjunction={true}
+            showOnlySubjunction={true}
+          />
+          <CommandMenu
+            open={filterMenuOpen}
+            setOpen={setFilterMenuOpen}
+            onSelect={handleSelection}
+            showOnlyjunction={true}
+            showOnlySubjunction={true}
+          />
+          <div className="flex flex-col md:flex-row justify-between items-center mb-4">
+            <button
+              className="px-4 py-2 bg-blue-500 text-white rounded mb-2 md:mb-0"
+              onClick={() => setCurrentWeek(getPreviousWeek())}
+            >
+              Previous Week
+            </button>
+            <h3 className="text-xl mb-2 font-bold md:mb-0">
+              {currentWeek.toLocaleDateString("en-IN", {
+                year: "numeric",
+                month: "2-digit",
+                day: "2-digit",
+              })}
+              {" - "}
+              {getNextWeek().toLocaleDateString("en-IN", {
+                year: "numeric",
+                month: "2-digit",
+                day: "2-digit",
+              })}
+            </h3>
+            <button
+              className="px-4 py-2 bg-blue-500 text-white rounded"
+              onClick={() => setCurrentWeek(getNextWeek())}
+            >
+              Next Week
+            </button>
+          </div>
+          <div className="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-4 mb-4">
+            <label htmlFor="filterPosition" className="font-semibold mb-2 md:mb-0">
+              Filter by Position:
+            </label>
+            <select
+              id="filterPosition"
+              value={filterPosition ?? "All"}
+              onChange={(e) =>
+                setFilterPosition(e.target.value === "All" ? null : e.target.value)
+              }
+              className="p-2 border rounded"
+            >
+              <option value="All">All</option>
+              <option value="TSI">TSI</option>
+              <option value="Constable">Constable</option>
+              <option value="Home Guard">Home Guard</option>
+            </select>
+            <input
+              type="text"
+              placeholder="Filter by Name"
+              value={filterName ?? ""}
+              onChange={(e) =>
+                setFilterName(e.target.value === "" ? null : e.target.value)
+              }
+              className="p-2 border rounded"
+            />
+            <input
+              type="text"
+              placeholder="Filter by Location"
+              value={filterLocation ?? ""}
+              onClick={handleLocationFilterClick}
+              readOnly
+              className="p-2 border rounded cursor-pointer"
+            />
+          </div>
+          {filteredSchedule.length > 0 ? (
+            <div className="overflow-x-auto">
+              <table className="min-w-full bg-white border">
+                <thead>
+                  <tr>
+                    <th className="py-2 px-4 border-b text-left">Role</th>
+                    <th className="py-2 px-4 border-b text-left">Name</th>
+                    <th className="py-2 px-4 border-b text-left">Location</th>
+                    <th className="py-2 px-4 border-b text-left">Shift</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {filteredSchedule.map((detail, index) => (
+                    <tr key={index}>
+                      <td className="py-2 px-4 border-b">{detail.personnel.role}</td>
+                      <td className="py-2 px-4 border-b">{detail.personnel.name}</td>
+                      <td className="py-2 px-4 border-b">
+                        <span
+                          onClick={() => handleLocationClick(index)}
+                          className="cursor-pointer text-blue-500"
+                        >
+                          {detail.junctions?.name ?? detail.sub_junctions?.name}
+                        </span>
+                      </td>
+                      <td className="py-2 px-4 border-b">{detail.shift ?? "All Day"}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <button
+                className="mt-4 px-4 py-2 bg-green-500 text-white rounded"
+                onClick={() => {}}
+              >
+                Save Changes
+              </button>
+            </div>
+          ) : (
+            <p>No schedule details available for the current week.</p>
+          )}
+        </CardContent>
+      </Card>
     </div>
   );
 };
--- components/component/Unassigned.tsx
+++ components/component/Unassigned.tsx
@@ -5,8 +5,7 @@ import { supabase } from "./supabase";
 
 const startDate = new Date(2024, 5, 24);
 export function Unassigned() {
-
-const [schedule, setSchedule] = useState<any[]>([]);
+  const [schedule, setSchedule] = useState<any[]>([]);
   const [currentWeek, setCurrentWeek] = useState<Date>(startDate);
   const [filterPosition, setFilterPosition] = useState<string | null>(null);
   const [filterName, setFilterName] = useState<string | null>(null);
@@ -17,10 +16,8 @@ const [schedule, setSchedule] = useState<any[]>([]);
   const [clickedCellIndex, setClickedCellIndex] = useState<number | null>(null);
   const [unAssigned, setUnAssigned] = useState([]);
 
-
-async function getUnassigned(currentDate) {
-
-const utcMidnight = new Date(
+  async function getUnassigned(currentDate) {
+    const utcMidnight = new Date(
       Date.UTC(
         currentDate.getFullYear(),
         currentDate.getMonth(),
@@ -29,20 +26,28 @@ const utcMidnight = new Date(
       )
     );
 
+    const { data: assignedPersonnel, error } = await supabase
+      .from("assignments")
+      .select("*,personnel(*)")
+      .lte("start_date", utcMidnight.toISOString())
+      .gte("end_date", utcMidnight.toISOString());
 
-    const { data:assignedPersonnel, error } = await supabase.from('assignments').select('*,personnel(*)').lte('start_date', utcMidnight.toISOString()).gte('end_date', utcMidnight.toISOString());
-    
-    const { data: personnel, error: personnelError } = await supabase.from('personnel').select('*');
+    const { data: personnel, error: personnelError } = await supabase
+      .from("personnel")
+      .select("*");
 
-  const assignedPersonnelIds = new Set(assignedPersonnel.map(assignment => assignment.personnel.id));
-  // Filter out the assigned personnel from the list of all personnel
-  const unassignedPersonnel = personnel.filter(person => !assignedPersonnelIds.has(person.id));
-  
-  console.log('unassigned', unassignedPersonnel);
-    
-  setUnAssigned(unassignedPersonnel);
+    const assignedPersonnelIds = new Set(
+      assignedPersonnel.map((assignment) => assignment.personnel.id)
+    );
+    // Filter out the assigned personnel from the list of all personnel
+    const unassignedPersonnel = personnel.filter(
+      (person) => !assignedPersonnelIds.has(person.id)
+    );
+
+    console.log("unassigned", unassignedPersonnel);
+
+    setUnAssigned(unassignedPersonnel);
   }
-
 
   useEffect(() => {
     getUnassigned(currentWeek);
@@ -105,110 +110,127 @@ const utcMidnight = new Date(
       setFilterMenuOpen(false);
     }
   };
-    
-    if (unAssigned.length == 0) {
-        return <p>Loading...</p>
-    }
 
-    
-      return    <Card id="unassigned">
-          <CardHeader>
-            <CardTitle>Rotation Schedule</CardTitle>
-          </CardHeader>
-          <CardContent>
-          <CommandMenu open={commandMenuOpen} setOpen={setCommandMenuOpen} onSelect={handleSelection} showOnlyjunction={true} showOnlySubjunction={true} />
-      <CommandMenu open={filterMenuOpen} setOpen={setFilterMenuOpen} onSelect={handleSelection} showOnlyjunction={true} showOnlySubjunction={true}/>
-      <div className="flex flex-col md:flex-row justify-between items-center mb-4">
-        <button
-          className="px-4 py-2 bg-blue-500 text-white rounded mb-2 md:mb-0"
-          onClick={() => setCurrentWeek(getPreviousWeek())}
-        >
-          Previous Week
-        </button>
-        <h3 className="text-xl mb-2 font-bold md:mb-0">
-          {currentWeek.toLocaleDateString("en-IN", {
-            year: "numeric",
-            month: "2-digit",
-            day: "2-digit",
-          })}
-          {" - "}
-          {getNextWeek().toLocaleDateString("en-IN", {
-            year: "numeric",
-            month: "2-digit",
-            day: "2-digit",
-          })}
-        </h3>
-        <button
-          className="px-4 py-2 bg-blue-500 text-white rounded"
-          onClick={() => setCurrentWeek(getNextWeek())}
-        >
-          Next Week
-        </button>
-      </div>
-      <div className="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-4 mb-4">
-        <label htmlFor="filterPosition" className="font-semibold mb-2 md:mb-0">
-          Filter by Position:
-        </label>
-        <select
-          id="filterPosition"
-          value={filterPosition ?? "All"}
-          onChange={(e) =>
-            setFilterPosition(e.target.value === "All" ? null : e.target.value)
-          }
-          className="p-2 border rounded"
-        >
-          <option value="All">All</option>
-          <option value="TSI">TSI</option>
-          <option value="Constable">Constable</option>
-          <option value="Home Guard">Home Guard</option>
-        </select>
-        <input
-          type="text"
-          placeholder="Filter by Name"
-          value={filterName ?? ""}
-          onChange={(e) =>
-            setFilterName(e.target.value === "" ? null : e.target.value)
-          }
-          className="p-2 border rounded"
+  if (unAssigned.length == 0) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <Card id="unassigned">
+      <CardHeader>
+        <CardTitle>Rotation Schedule</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <CommandMenu
+          open={commandMenuOpen}
+          setOpen={setCommandMenuOpen}
+          onSelect={handleSelection}
+          showOnlyjunction={true}
+          showOnlySubjunction={true}
         />
-        <input
-          type="text"
-          placeholder="Filter by Location"
-          value={filterLocation ?? ""}
-          onClick={handleLocationFilterClick}
-          readOnly
-          className="p-2 border rounded cursor-pointer"
+        <CommandMenu
+          open={filterMenuOpen}
+          setOpen={setFilterMenuOpen}
+          onSelect={handleSelection}
+          showOnlyjunction={true}
+          showOnlySubjunction={true}
         />
-      </div>
-      {unAssigned.length > 0 ? (
-        <div className="overflow-x-auto">
-          <table className="min-w-full bg-white border">
-            <thead>
-              <tr>
-                <th className="py-2 px-4 border-b text-left">Role</th>
-                <th className="py-2 px-4 border-b text-left">Name</th>
-              </tr>
-            </thead>
-            <tbody>
-              {unAssigned.map((detail, index) => (
-                <tr key={index}>
-                  <td className="py-2 px-4 border-b">{detail.role}</td>
-                  <td className="py-2 px-4 border-b">{detail.name}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+        <div className="flex flex-col md:flex-row justify-between items-center mb-4">
           <button
-            className="mt-4 px-4 py-2 bg-green-500 text-white rounded"
-            onClick={() => {}}
+            className="px-4 py-2 bg-blue-500 text-white rounded mb-2 md:mb-0"
+            onClick={() => setCurrentWeek(getPreviousWeek())}
           >
-            Save Changes
+            Previous Week
+          </button>
+          <h3 className="text-xl mb-2 font-bold md:mb-0">
+            {currentWeek.toLocaleDateString("en-IN", {
+              year: "numeric",
+              month: "2-digit",
+              day: "2-digit",
+            })}
+            {" - "}
+            {getNextWeek().toLocaleDateString("en-IN", {
+              year: "numeric",
+              month: "2-digit",
+              day: "2-digit",
+            })}
+          </h3>
+          <button
+            className="px-4 py-2 bg-blue-500 text-white rounded"
+            onClick={() => setCurrentWeek(getNextWeek())}
+          >
+            Next Week
           </button>
         </div>
-      ) : (
-        <p>No schedule details available for the current week.</p>
-      )}
-          </CardContent>
-        </Card>
-
+        <div className="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-4 mb-4">
+          <label
+            htmlFor="filterPosition"
+            className="font-semibold mb-2 md:mb-0"
+          >
+            Filter by Position:
+          </label>
+          <select
+            id="filterPosition"
+            value={filterPosition ?? "All"}
+            onChange={(e) =>
+              setFilterPosition(
+                e.target.value === "All" ? null : e.target.value
+              )
+            }
+            className="p-2 border rounded"
+          >
+            <option value="All">All</option>
+            <option value="TSI">TSI</option>
+            <option value="Constable">Constable</option>
+            <option value="Home Guard">Home Guard</option>
+          </select>
+          <input
+            type="text"
+            placeholder="Filter by Name"
+            value={filterName ?? ""}
+            onChange={(e) =>
+              setFilterName(e.target.value === "" ? null : e.target.value)
+            }
+            className="p-2 border rounded"
+          />
+          <input
+            type="text"
+            placeholder="Filter by Location"
+            value={filterLocation ?? ""}
+            onClick={handleLocationFilterClick}
+            readOnly
+            className="p-2 border rounded cursor-pointer"
+          />
+        </div>
+        {unAssigned.length > 0 ? (
+          <div className="overflow-x-auto">
+            <table className="min-w-full bg-white border">
+              <thead>
+                <tr>
+                  <th className="py-2 px-4 border-b text-left">Role</th>
+                  <th className="py-2 px-4 border-b text-left">Name</th>
+                </tr>
+              </thead>
+              <tbody>
+                {unAssigned.map((detail, index) => (
+                  <tr key={index}>
+                    <td className="py-2 px-4 border-b">{detail.role}</td>
+                    <td className="py-2 px-4 border-b">{detail.name}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <button
+              className="mt-4 px-4 py-2 bg-green-500 text-white rounded"
+              onClick={() => {}}
+            >
+              Save Changes
+            </button>
+          </div>
+        ) : (
+          <p>No schedule details available for the current week.</p>
+        )}
+      </CardContent>
+    </Card>
+  );
 }
